4864437437e3e9e5d846687fca7c882b419977bc
mike stedman <barbeque@users.noreply.github.com>
2019-04-10 21:37:45 -0600

    Draft 2

--- reset.asm
+++ reset.asm
@@ -1,9 +1,10 @@
 reset:
-    mov al, 0x37
-    out 0x0f, al ; SHUT0 = 0
-    out 0x0b, al ; SHUT1 = 0
-    mov al, 0xf0
-    out 0x00, al ; activate RESET input of CPU
+    mov al, 0x0f
+    out 0x37, al ; SHUT0 = 0
+    mov al, 0x0b
+    out 0x37, al ; SHUT1 = 0
+    mov al, 0x00
+    out 0xf0, al ; activate RESET input of CPU
 
 .start:
     jmp reset
