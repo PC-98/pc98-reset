82b3bbc816ce94ac334cd43aa4070600a7203745
mike stedman <barbeque@users.noreply.github.com>
2019-04-10 21:40:26 -0600

    Added some boilerplate as per SO

--- reset.asm
+++ reset.asm
@@ -1,3 +1,6 @@
+[section] .text
+    global _start
+
 reset:
     mov al, 0x0f
     out 0x37, al ; SHUT0 = 0
@@ -6,5 +9,5 @@ reset:
     mov al, 0x00
     out 0xf0, al ; activate RESET input of CPU
 
-.start:
+_start:
     jmp reset
